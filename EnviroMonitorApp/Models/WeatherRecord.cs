c686a7e03c45da61eadec0bfb181125fe0cfc449
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2025-04-25 16:37:13 +0100

    Refactored WeatherRecord.cs for database context
    Signed-off-by: Reece Smith <40872343+reeceasmith@users.noreply.github.com>

--- EnviroMonitorApp/Models/WeatherRecord.cs
+++ EnviroMonitorApp/Models/WeatherRecord.cs
@@ -1,8 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EnviroMonitorApp.Models;
 
+[Table("weather_records")]
+[PrimaryKey(nameof(DateTime))]
 public class WeatherRecord
 {
-    public string? Date { get; set; }
-    public double Temperature { get; set; }
-    public double Humidity { get; set; }
+    [Column("record_datetime")]
+    public DateTime DateTime { get; set; }
+
+    [Column("temperature")]
+    public float Temperature { get; set; }
+
+    [Column("relative_humidity")]
+    public int RelativeHumidity { get; set; }
+
+    [Column("wind_speed")]
+    public float WindSpeed { get; set; }
+
+    [Column("wind_direction")]
+    public int WindDirection { get; set; }
 }
