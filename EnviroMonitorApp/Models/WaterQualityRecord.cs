a83d71c3a79395ec091d536e8ffa21e6d182537e
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2025-04-25 16:43:39 +0100

    Refactored WaterQualityRecord.cs to match database
    Signed-off-by: Reece Smith <40872343+reeceasmith@users.noreply.github.com>

--- EnviroMonitorApp/Models/WaterQualityRecord.cs
+++ EnviroMonitorApp/Models/WaterQualityRecord.cs
@@ -1,10 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace EnviroMonitorApp.Models;
 
+[Table("water_quality_records")]
+[PrimaryKey(nameof(DateTime))]
 public class WaterQualityRecord
 {
+    [Column("record_datetime")]
     public DateTime DateTime { get; set; }
+    [Column("nitrate")]
     public double Nitrate { get; set; }
+    [Column("nitrite")]
     public double Nitrite { get; set; }
+    [Column("phosphate")]
     public double Phosphate { get; set; }
+    [Column("ec")]
     public double EC { get; set; } // E. coli or similar measurement
 }
