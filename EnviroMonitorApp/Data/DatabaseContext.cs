3e6043bcfe66c25a7e539f90604722e132452a2b
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2025-04-25 12:21:37 +0100

    Created DatabaseContext.cs
    Signed-off-by: Reece Smith <40872343+reeceasmith@users.noreply.github.com>

---
+++ EnviroMonitorApp/Data/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using EnviroMonitorApp.Models;
+
+
+namespace EnviroMonitorApp.Data;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext()
+    { }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    { }
+    
+    
+}
