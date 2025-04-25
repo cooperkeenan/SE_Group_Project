231b294510cb4a784704f90093f25ec4e1de916b
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2025-04-25 16:18:53 +0100

    Added sample data as insert sql scripts and updated create/populate sql scripts to insert data
    Signed-off-by: Reece Smith <40872343+reeceasmith@users.noreply.github.com>

---
+++ SQL/scripts/create_database.sql
@@ -0,0 +1,95 @@
+USE master;
+-- Drop any existing database
+IF DB_ID('enviromonitor') IS NOT NULL
+BEGIN
+    ALTER DATABASE enviromonitor
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE enviromonitor;
+END
+GO
+
+-- Create a new instance of the database
+CREATE DATABASE enviromonitor;
+GO
+
+USE enviromonitor;
+GO
+
+CREATE LOGIN enviromonitor_app WITH PASSWORD='${DB_PASSWORD}';
+CREATE USER enviromonitor_app for login enviromonitor_app;
+GRANT CONTROL ON DATABASE::enviromonitor to enviromonitor_app;
+GO
+
+-- Create tables
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+    username VARCHAR(31) UNIQUE,
+    fullname VARCHAR(63),
+    password_hash VARCHAR(255)
+);
+GO
+
+CREATE TABLE permissions (
+    id INT PRIMARY KEY,
+    permission_name VARCHAR(32) UNIQUE,
+    permission_description TEXT
+);
+GO
+
+CREATE TABLE user_permissions (
+    user_id INT,
+    permission_id INT,
+
+    PRIMARY KEY (user_id, permission_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (permission_id) REFERENCES permissions(id)
+);
+GO
+
+
+CREATE TABLE metadata (
+    category VARCHAR(32),
+    quantity VARCHAR(64),
+    symbol VARCHAR(8),
+    unit VARCHAR(16),
+    unit_description VARCHAR(64),
+    measurement_frequency VARCHAR(32),
+    safe_level DECIMAL(4,1),
+    reference VARCHAR(255),
+    sensor VARCHAR(16),
+    data_url VARCHAR(255),
+
+    PRIMARY KEY (category, quantity)
+);
+
+
+CREATE TABLE air_quality_records (
+    record_date DATE,
+    record_time TIME,
+    nitrogen_dioxide FLOAT,
+    sulphur_dioxide FLOAT,
+    pm25 FLOAT,
+    pm10 FLOAT,
+
+    PRIMARY KEY (record_date, record_time)
+);
+
+CREATE TABLE water_quality_records (
+    record_date DATE,
+    record_time TIME,
+    nitrate FLOAT,
+    nitrite FLOAT,
+    phosphate FLOAT,
+    ec FLOAT
+
+    PRIMARY KEY (record_date, record_time)
+);
+
+CREATE TABLE weather_records (
+    record_datetime DATETIME PRIMARY KEY,
+    temperature FLOAT,
+    relative_humidity TINYINT,
+    wind_speed FLOAT,
+    wind_direction SMALLINT
+);
+GO
