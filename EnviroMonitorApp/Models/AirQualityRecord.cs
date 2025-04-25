2a09ae7eb9db17c5172edf9e242cd12ee2cfb8aa
Reece Smith <40872343+reeceasmith@users.noreply.github.com>
2025-04-25 16:45:14 +0100

    Refactored AirQualityRecord.cs to match database
    Signed-off-by: Reece Smith <40872343+reeceasmith@users.noreply.github.com>

--- EnviroMonitorApp/Models/AirQualityRecord.cs
+++ EnviroMonitorApp/Models/AirQualityRecord.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace EnviroMonitorApp.Models;
 
+[Table("air_quality_records")]
+[PrimaryKey(nameof(DateTime))]
 public class AirQualityRecord
 {
+    [Column("datetime")]
     public DateTime DateTime { get; set; }
+    [Column("nitrogen_dioxide")]
     public double NitrogenDioxide { get; set; }
+    [Column("sulphur_dioxide")]
     public double SulphurDioxide { get; set; }
+    [Column("pm25")]
     public double PM25 { get; set; }
+    [Column("pm10")]
     public double PM10 { get; set; }
 }
