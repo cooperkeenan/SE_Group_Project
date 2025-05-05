2bad68ba07ec965f218dee4dd8c57beb8a6fb097
KaiWMalcolm <157699441+KaIWMalcolm@users.noreply.github.com>
2025-05-05 17:45:23 +0100

    Delete EnviroMonitorApp directory

--- EnviroMonitorApp/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace EnviroMonitorApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
